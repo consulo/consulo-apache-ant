9819d46b278cc159b540ebd5bdf4cef81ce6a6bd
VISTALL <vistall.valeriy@gmail.com>
2015-07-21 12:29:49 +0300

    import libraries from lib

--- src/org/mustbe/consulo/apache/ant/sdk/AntSdkType.java
+++ src/org/mustbe/consulo/apache/ant/sdk/AntSdkType.java
@@ -15,58 +15,136 @@
  */
 package org.mustbe.consulo.apache.ant.sdk;
 
-import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 import javax.swing.Icon;
 
-import org.mustbe.consulo.apache.ant.util.AntVersionUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.mustbe.consulo.apache.ant.ApacheAntIcons;
+import org.mustbe.consulo.apache.ant.util.AntVersionUtil;
+import com.intellij.ide.plugins.IdeaPluginDescriptor;
+import com.intellij.ide.plugins.PluginManager;
+import com.intellij.ide.plugins.cl.PluginClassLoader;
+import com.intellij.openapi.projectRoots.Sdk;
+import com.intellij.openapi.projectRoots.SdkModificator;
 import com.intellij.openapi.projectRoots.SdkType;
+import com.intellij.openapi.roots.OrderRootType;
+import com.intellij.openapi.roots.types.BinariesOrderRootType;
+import com.intellij.openapi.vfs.LocalFileSystem;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.vfs.util.ArchiveVfsUtil;
 
 /**
  * @author VISTALL
  * @since 15:52/19.05.13
  */
-public class AntSdkType extends SdkType {
-  public AntSdkType() {
-    super("Apache Ant");
-  }
+public class AntSdkType extends SdkType
+{
+	public AntSdkType()
+	{
+		super("APACHE_ANT");
+	}
 
-  @Override
-  public Icon getIcon() {
-    return ApacheAntIcons.AntInstallation;
-  }
+	@NotNull
+	@Override
+	public Collection<String> suggestHomePaths()
+	{
+		PluginClassLoader classLoader = (PluginClassLoader) AntSdkType.class.getClassLoader();
+		IdeaPluginDescriptor plugin = PluginManager.getPlugin(classLoader.getPluginId());
+		assert plugin != null;
+		VirtualFile fileByIoFile = LocalFileSystem.getInstance().findFileByIoFile(plugin.getPath());
+		if(fileByIoFile == null)
+		{
+			return Collections.emptyList();
+		}
+		VirtualFile dist = fileByIoFile.findChild("dist");
+		if(dist == null)
+		{
+			return Collections.emptyList();
+		}
+		VirtualFile[] children = dist.getChildren();
+		List<String> list = new ArrayList<String>(children.length);
+		for(VirtualFile child : children)
+		{
+			list.add(child.getPath());
+		}
+		return list;
+	}
 
-  @Nullable
-  @Override
-  public Icon getGroupIcon() {
-    return ApacheAntIcons.AntGroup;
-  }
+	@Override
+	public boolean canCreatePredefinedSdks()
+	{
+		return true;
+	}
 
-  @Override
-  public boolean isValidSdkHome(String path) {
-    return AntVersionUtil.getVersion(path) != null;
-  }
+	@Override
+	public void setupSdkPaths(Sdk sdk)
+	{
+		SdkModificator sdkModificator = sdk.getSdkModificator();
+		VirtualFile homeDirectory = sdk.getHomeDirectory();
+		VirtualFile lib = homeDirectory.findChild("lib");
+		if(lib != null)
+		{
+			for(VirtualFile virtualFile : lib.getChildren())
+			{
+				VirtualFile archiveRootForLocalFile = ArchiveVfsUtil.getArchiveRootForLocalFile(virtualFile);
+				if(archiveRootForLocalFile != null)
+				{
+					sdkModificator.addRoot(archiveRootForLocalFile, BinariesOrderRootType.getInstance());
+				}
+			}
+		}
+		sdkModificator.commitChanges();
+	}
 
-  @Nullable
-  @Override
-  public String getVersionString(String sdkHome) {
-    final String version = AntVersionUtil.getVersion(sdkHome);
+	@Override
+	public Icon getIcon()
+	{
+		return ApacheAntIcons.AntInstallation;
+	}
 
-    return version == null ? "0.0" : version;
-  }
+	@Nullable
+	@Override
+	public Icon getGroupIcon()
+	{
+		return ApacheAntIcons.AntGroup;
+	}
 
-  @Override
-  public String suggestSdkName(String currentSdkName, String sdkHome) {
-    File file = new File(sdkHome);
-    return file.getName();
-  }
+	@Override
+	public boolean isValidSdkHome(String path)
+	{
+		return AntVersionUtil.getVersion(path) != null;
+	}
 
-  @NotNull
-  @Override
-  public String getPresentableName() {
-    return getName();
-  }
+	@Nullable
+	@Override
+	public String getVersionString(String sdkHome)
+	{
+		final String version = AntVersionUtil.getVersion(sdkHome);
+
+		return version == null ? "0.0" : version;
+	}
+
+	@Override
+	public String suggestSdkName(String currentSdkName, String sdkHome)
+	{
+		return getPresentableName() + " " + getVersionString(sdkHome);
+	}
+
+	@Override
+	public boolean isRootTypeApplicable(OrderRootType type)
+	{
+		return type == BinariesOrderRootType.getInstance();
+	}
+
+	@NotNull
+	@Override
+	public String getPresentableName()
+	{
+		return "Apache Ant";
+	}
 }
