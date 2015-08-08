40851dab7c31f8fad0d8157b84ba589a8e2fbca3
VISTALL <vistall.valeriy@gmail.com>
2015-08-08 04:00:42 +0300

    override

--- src/com/intellij/lang/ant/config/impl/AntInstallationClassLoaderHolder.java
+++ src/com/intellij/lang/ant/config/impl/AntInstallationClassLoaderHolder.java
@@ -15,42 +15,50 @@
  */
 package com.intellij.lang.ant.config.impl;
 
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.util.config.AbstractProperty;
-
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-public class AntInstallationClassLoaderHolder extends ClassLoaderHolder {
-  private static final Logger LOG = Logger.getInstance("#com.intellij.lang.ant.config.impl.AntInstallationClassLoaderHolder");
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.projectRoots.Sdk;
 
-  public AntInstallationClassLoaderHolder(AbstractProperty.AbstractPropertyContainer options) {
-    super(options);
-  }
+public class AntInstallationClassLoaderHolder extends ClassLoaderHolder
+{
+	private static final Logger LOG = Logger.getInstance("#com.intellij.lang.ant.config.impl.AntInstallationClassLoaderHolder");
 
-  protected ClassLoader buildClasspath() {
-    final ArrayList<File> files = new ArrayList<File>();
-    // ant installation jars
-    final List<AntClasspathEntry> cp = AntInstallation.CLASS_PATH.get(myOptions);
-    for (final AntClasspathEntry entry : cp) {
-      entry.addFilesTo(files);
-    }
+	public AntInstallationClassLoaderHolder(Sdk sdk)
+	{
+		super(sdk, null);
+	}
 
-    // jars from user home
-    files.addAll(AntBuildFileImpl.getUserHomeLibraries());
+	@Override
+	protected ClassLoader buildClasspath()
+	{
+		final ArrayList<File> files = new ArrayList<File>();
+		// ant installation jars
+		final List<AntClasspathEntry> cp = AntInstallation.CLASS_PATH.get(myOptions);
+		for(final AntClasspathEntry entry : cp)
+		{
+			entry.addFilesTo(files);
+		}
 
-    final List<URL> urls = new ArrayList<URL>(files.size());
-    for (File file : files) {
-      try {
-        urls.add(file.toURI().toURL());
-      }
-      catch (MalformedURLException e) {
-        LOG.debug(e);
-      }
-    }
-    return new AntResourcesClassLoader(urls, null, true, false);
-  }
+		// jars from user home
+		files.addAll(AntBuildFileImpl.getUserHomeLibraries());
+
+		final List<URL> urls = new ArrayList<URL>(files.size());
+		for(File file : files)
+		{
+			try
+			{
+				urls.add(file.toURI().toURL());
+			}
+			catch(MalformedURLException e)
+			{
+				LOG.debug(e);
+			}
+		}
+		return new AntResourcesClassLoader(urls, null, true, false);
+	}
 }
