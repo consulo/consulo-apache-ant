7da33d2f821dc7f37319d62e86a817558c9945a5
VISTALL <vistall.valeriy@gmail.com>
2015-08-08 04:34:02 +0300

    fixed eap name

--- src/com/intellij/lang/ant/config/execution/AntMessageCustomizer.java
+++ src/com/intellij/lang/ant/config/execution/AntMessageCustomizer.java
@@ -1,11 +1,11 @@
 package com.intellij.lang.ant.config.execution;
 
-import com.intellij.openapi.extensions.ExtensionPointName;
 import org.jetbrains.annotations.Nullable;
+import com.intellij.openapi.extensions.ExtensionPointName;
 
 public abstract class AntMessageCustomizer {
 
-  public static final ExtensionPointName<AntMessageCustomizer> EP_NAME = ExtensionPointName.create("AntSupport.AntMessageCustomizer");
+  public static final ExtensionPointName<AntMessageCustomizer> EP_NAME = ExtensionPointName.create("org.mustbe.consulo.ant.messageCustomizer");
 
   @Nullable
   public AntMessage createCustomizedMessage(String text, int priority) {
